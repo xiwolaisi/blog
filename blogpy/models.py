2891561ce219bcc3302ff74ee2f53bc1ff828532
danchaofan-duofangyan <31961609+danchaofan-duofangyan@users.noreply.github.com>
2017-09-19 21:14:32 +0800

    update

--- blogpy/models.py
+++ blogpy/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
+import markdown
+from django.utils.html import strip_tags
 
 # Create your models here.
 class Category(models.Model):
@@ -22,6 +24,16 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1;
         self.save(update_fields=['views'])
+    def save(self, *args,**kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(
+                extensions=[
+                    'markdown.extensions.extra',
+                    'markdown.extensions.codehilite',
+
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post,self).save(*args,**kwargs)
     #标题
     title = models.CharField(max_length=100)
     #正文
